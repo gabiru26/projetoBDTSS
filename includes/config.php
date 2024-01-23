5ce07fa4bcc3c9778007e19b52f38b530bd6f68a
gabiru26 <gabriela.ribeiro@ua.pt>
2024-01-23 11:10:05 +0000

    commit

--- includes/config.php
+++ includes/config.php
@@ -3,5 +3,13 @@
 define('DB_HOST', 'localhost');
 define('DB_USER', 'root');
 define('DB_PASSWORD', '');
-define('DB_NAME', 'mydb');
+define('DB_NAME', 'projeto_BDTSS');
+
+
+$conn = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+?>
 ?>
